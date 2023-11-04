953ba7751db1f550e3b1920b5b40d98df34728b8
TrueRaisins <idanik.ishot@gmail.com>
2023-11-04 13:16:43 +0300

    initial commit

---
+++ src/main/java/Einmaleins/Einmaleins.java
@@ -0,0 +1,16 @@
+package Einmaleins;
+
+public class Einmaleins {
+    private int size = 10;
+    public Einmaleins(int size){
+        this.size = size;
+    }
+    public void calculate() {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= size; j++) {
+                System.out.print(i * j + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/Einmaleins/Main.java
@@ -0,0 +1,10 @@
+package Einmaleins;
+
+public class Main {
+    public static void main (String[] args){
+        Einmaleins einmaleins = new Einmaleins(10);
+        einmaleins.calculate();
+
+        System.out.println(einmaleins);
+    }
+}
