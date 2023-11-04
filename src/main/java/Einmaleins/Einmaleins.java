a17ec5e9259169241b9163b33379ca1d6e705234
TrueRaisins <idanik.ishot@gmail.com>
2023-11-04 13:17:42 +0300

    initial commit

--- src/main/java/Einmaleins/Einmaleins.java
+++ src/main/java/Einmaleins/Einmaleins.java
@@ -2,9 +2,11 @@ package Einmaleins;
 
 public class Einmaleins {
     private int size = 10;
-    public Einmaleins(int size){
+
+    public Einmaleins(int size) {
         this.size = size;
     }
+
     public void calculate() {
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= size; j++) {
